216bc70bd9c14a475b9e91e4e3d9cc5dce6b2c96
emilfunke <emil.funke98@gmail.com>
2022-03-21 16:31:56 +0100

    yellow circle on black background moving left to right

--- eye_movement_dot.py
+++ eye_movement_dot.py
@@ -1,2 +1,39 @@
-import pandas as pd
+# found on https://www.geeksforgeeks.org/draw-moving-object-using-turtle-in-python/
+
+import turtle
+from screeninfo import get_monitors
+
+
+def moving_obj(move):
+    move.fillcolor('yellow')
+    move.begin_fill()
+    move.circle(25)
+    move.end_fill()
+
+
+if __name__ == "__main__":
+    screen = turtle.Screen()
+    for m in get_monitors():
+        height = m.height
+        width = m.width
+    print(height, width)
+    screen.setup(width, height)
+    screen.bgcolor('black')
+    screen.tracer(0)
+    move = turtle.Turtle()
+    move.color('yellow')
+    move.speed(10)
+    move.width(5)
+    move.hideturtle()
+    move.penup()
+    move.goto(-width/2 + 30, height/2 -80)
+    move.pendown()
+
+
+while True:
+    move.clear()
+    moving_obj(move)
+    screen.update()
+    print(move.pos())
+    move.forward(2)
 
