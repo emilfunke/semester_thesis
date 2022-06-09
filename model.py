014863f92a7e534efe9e10e2064b3df48ba531be
Emil Funke <emil.funke98@gmail.com>
2022-06-09 14:54:24 +0200

    get time modified from picture files

--- model.py
+++ model.py
@@ -1,21 +1,16 @@
 import numpy as np
+import os
 import cv2
-from PIL import Image
-from PIL.ExifTags import TAGS
+from datetime import datetime, timezone
+from dateutil import tz
 
 # opencv face and eye detection of pictures
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+# get date and time of picture creation from metadata
 image_name = "000000_fc_46_processed.jpg"
-image = Image.open("camera_output/2022-05-27_12-27-07_spiral1/" + image_name)
-print(image.filename)
-exif_data = image.getexif()
-print(exif_data)
-for tag_id in exif_data:
-    tag = TAGS.get(tag_id, tag_id)
-    data = exif_data.get(tag_id)
-    if isinstance(data, bytes):
-        data = data.decode()
-    print("pisser")
+mtime = os.path.getmtime("camera_output/2022-05-27_12-27-07_spiral1/" + image_name)
+time_utc = datetime.fromtimestamp(mtime, tz=timezone.utc)
+to_zone = tz.tzlocal()
+time = time_utc.astimezone(to_zone)
